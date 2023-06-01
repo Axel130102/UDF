eee7b06e0a10f8568e9a8bb7fffffcdbc8f6167b
FerciCrack <135089244+FerciCrack@users.noreply.github.com>
2023-06-01 09:34:14 -0600

    Subo códigos en lenguaje PHP

---
+++ ejercicioUno.php
@@ -0,0 +1,18 @@
+
+<?php
+echo "Imprimir valores del 0 - 100";
+echo "\n Acendente";
+
+for($a = 0; $a <= 100; $a++){
+    print "<p>$a</p>\n";
+
+    }
+
+echo "Descendente";
+
+for($b = 100; $b >= 0; $b--){
+    print "<p>$b</p>\n";
+
+    }
+
+?>
