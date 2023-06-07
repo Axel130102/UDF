0ec90fd2d60392e4e93efd8e495565af3b8b25f7
$Anthony$ <135397498+Anth0ny43@users.noreply.github.com>
2023-06-06 18:34:06 -0600

    Create ejer4.php

---
+++ php2/ejer4.php
@@ -0,0 +1,13 @@
+<?php
+echo "Números en orden ascendente:" . PHP_EOL;
+for ($i = 0; $i <= 100; $i++) {
+    echo $i . " ";
+}
+echo PHP_EOL;
+
+echo "Números en orden descendente:" . PHP_EOL;
+for ($i = 100; $i >= 0; $i--) {
+    echo $i . " ";
+}
+echo PHP_EOL;
+?>
