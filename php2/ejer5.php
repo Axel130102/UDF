0015a76c2c77bbe3538e675c9eb0952e8f35f517
$Anthony$ <135397498+Anth0ny43@users.noreply.github.com>
2023-06-06 18:34:29 -0600

    Create ejer5.php

---
+++ php2/ejer5.php
@@ -0,0 +1,13 @@
+<?php
+echo "Los primeros 100 números pares:" . PHP_EOL;
+for ($i = 0; $i < 200; $i += 2) {
+    echo $i . " ";
+}
+echo PHP_EOL;
+
+echo "Los primeros 100 números impares:" . PHP_EOL;
+for ($i = 1; $i < 200; $i += 2) {
+    echo $i . " ";
+}
+echo PHP_EOL;
+?>
